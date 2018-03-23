c7a2e66cd46e54c160a1c4411ea5022f0a8ddaac
Szymon Bialkowski <bialkowski.sz@gmail.com>
2018-03-23 21:30:16 +0000

    Completed Queens Attack

--- Algorithms/Implementation/Queens Attack/solution.py
+++ Algorithms/Implementation/Queens Attack/solution.py
@@ -1,4 +1,17 @@
-#!/bin/python
+#!/bin/python3
+
+# Copyright (c) 2018 Szymon Bialkowski
+# Authors: Szymon Bialkowski
+# Date: 23/03/2018
+
+"""
+Calculate max attacks in each direction in constant time
+For each obstacle find if on attack lines
+If blocks left after obstacle then put into max attack variables
+Add all variables
+
+Complexity: O(k) time and O(1) space
+"""
 
 import sys
 
@@ -6,46 +19,70 @@ def queensAttack(n, k, rQ, cQ, obstacles):
     if n == 0 or n == 1:
         return 0
 
-    top, bottom, left, right = [ n, 0, 0, n ]
-    # Whichever one is closer to the edge will collied
-    topRight = n - cQ if cQ >= rQ else n - rQ
-    topLeft  = cQ - 1 if cQ <= rQ else rQ - 1
-    bottomLeft, bottomRight  = [ 0, 0 ]
+    top, bottom, left, right = [ n-rQ, rQ-1, cQ-1, n-cQ ]
+    topRight    = top    if top <= right    else right
+    topLeft     = top    if top <= left     else left
+    bottomLeft  = bottom if bottom <= left  else left
+    bottomRight = bottom if bottom <= right else right
 
     for obstacle in obstacles:
-        if cQ == obstacle[1]:
-            if rQ < obstacle[0]:
-                if top > obstacles[0]:
-                    top = obstacles[0]
-            else:
-                if bottom < obstacle[0]:
-                    bottom = obstacles[0]
-        elif rQ == obstacle[0]:
-            if cQ < obstacles[1]:
-                if right > obstacles[1]:
-                    right = obstacles[1]
-            else:
-                if left < obstacles[1]:
-                    left = obstacles[1]
 
         queensDiagonal = obstacle[0] - rQ
 
-        elif queensDiagonal == obstacle[1] - cQ:
-            if queensDiagonal > 0:
-                if topRight > queensDiagonal:
-                    topRight = queensDiagonal
+        #   Top and bottom
+        if cQ == obstacle[1]:
+            if rQ < obstacle[0]:
+                blocksLeft = (obstacles[0]-(rQ-1))
+                if top > blocksLeft:
+                    top = blocksLeft
             else:
-                if bottomLeft < obstacle[0] - rQ:
-                    bottomLeft = queensDiagonal
+                blocksLeft = (rQ-obstacles[0]-1)
+                if bottom > blocksLeft:
+                    bottom = blocksLeft
+
+        #   Left and right
+        elif rQ == obstacle[0]:
+            if cQ < obstacles[1]:
+                blocksLeft = (obstacles[1]-cQ-1)
+                if right > blocksLeft:
+                    right = blocksLeft
+            else:
+                blocksLeft = (cQ-obstacles[1]-1)
+                if left > blocksLeft:
+                    left = blocksLeft
+
+        #   TopRight and BottomRight diagonals
+        elif queensDiagonal == (obstacle[1] - cQ):
+            if queensDiagonal > 0:
+                blocksLeft = queensDiagonal-1
+                if topRight > blocksLeft:
+                    topRight = blocksLeft
+            else:
+                blocksLeft = abs(queensDiagonal)-1
+                if bottomLeft > blocksLeft:
+                    bottomLeft = blocksLeft
+
+        #   TopLeft and BottomRight diagonals
+        elif (obstacle[0] + obstacle[1]) == (rQ + cQ):
+            if rQ < obstacle[0]:
+                blocksLeft = (obstacle[0]-rQ-1)
+                if topLeft > blocksLeft:
+                    topLeft = blocksLeft
+            else:
+                blocksLeft = (rQ-obstacle[0]-1)
+                if bottomRight > blocksLeft:
+                    bottomRight = blocksLeft
+
+    return top + bottom + left + right + \
+           topLeft + topRight + bottomRight + bottomLeft
 
 
 if __name__ == "__main__":
-    n, k      = [ int(i) for i in input().strip().split(' ') ]
-    r_q, c_q  = [ int(i) for i in input().strip().split(' ') ]
+    n, k      = [ int(i) for i in raw_input().strip().split(' ') ]
+    r_q, c_q  = [ int(i) for i in raw_input().strip().split(' ') ]
     obstacles = [ ]
     for obstacle in range(k):
-        obstacles_temp = map(int, input().split().strip(' '))
+        obstacles_temp = [ int(i) for i in raw_input().strip().split(' ') ]
         obstacles.append(obstacles_temp)
     result = queensAttack(n, k, r_q, c_q, obstacles)
     print(result)
-
