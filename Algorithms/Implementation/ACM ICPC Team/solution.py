a0b5d601264112f90e011f8b614edf7b3705fbdc
BialkowskiSz <bialkowski.sz@gmail.com>
2018-04-22 22:19:45 +0100

    Solution.

---
+++ Algorithms/Implementation/ACM ICPC Team/solution.py
@@ -0,0 +1,40 @@
+
+#!/bin/python
+
+import sys, heapq
+
+def acmTeam(topic):
+    if not topic:
+        return (0,0)
+    
+    heap = []
+    length = len(topic)
+    stringLength = len(topic[0])
+    
+    for i in range(length-1):
+        for j in range(i+1, length):
+            counter = 0
+            for z in range(stringLength):
+                if topic[i][z] == '1' or topic[j][z] == '1':
+                    counter += 1
+            heapq.heappush(heap, -1*counter)
+    
+    counter = 1
+    biggest = heapq.heappop(heap)
+    while biggest == heapq.heappop(heap):
+        counter += 1
+    
+    return (abs(biggest), counter)
+
+if __name__ == "__main__":
+    n, m = raw_input().strip().split(' ')
+    n, m = [int(n), int(m)]
+    topic = []
+    topic_i = 0
+    for topic_i in xrange(n):
+        topic_t = str(raw_input().strip())
+        topic.append(topic_t)
+    result = acmTeam(topic)
+    print "\n".join(map(str, result))
+
+
